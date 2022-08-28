3a2b794372e40ab6a92e1872652caed086b18ab7
oyedele oreofe <oyedeleoreofe@gmail.com>
2022-08-28 16:57:34 +0100

    phew

--- sign.js
+++ sign.js
@@ -1,32 +1,27 @@
 
-allMembers = []
-// if(localStorage.allMembers){
-// //    allMembers = localStorage.getItem(JSON.stringify("savaBankDetails"))
-// }
-const signUp =()=>{
-registeredStudents = {
-    fName: firstName.value,
-    lName: lastName.value,
-    eMail: email.value,
-    password: passWord.value,
 
+var allMembers = []
+if (localStorage.saveBankDetails) {
+    allMembers = JSON.parse(localStorage.getItem("saveBankDetails"))
 }
+const signUp = () => {
+    registeredStudents = {
+        fName: firstName.value,
+        lName: lastName.value,
+        eMail: email.value,
+        password: passWord.value
 
-firstName.value == ""
- if(firstName.value != "" ){
- 
-    allMembers.push(registeredStudents.fName)
-    console.log(allMembers)
-    alert("saved")
+    }
+    if (firstName.value != "") {
+        console.log(allMembers)
+         window.location.href = "accountnumber.html", allMembers.push(registeredStudents)
+        firstName.value = "", lastName.value = "", email.value  = "", passWord.value = ""
+           alert("saved")
 
-
-
- }
-else{
-    alert("input the following  info")
-}
-
-
-   localStorage.setItem("savaBankDetails", JSON.stringify(allMembers))
+    }
+    else {
+        alert("input the following  info")
+    }
+    localStorage.setItem("saveBankDetails", JSON.stringify(allMembers))
 }
 
