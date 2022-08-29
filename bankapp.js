ce1167731f86fe2219fed34f73645375c5d8a3cd
ikhally <111930017+ikhally@users.noreply.github.com>
2022-08-29 12:26:13 +0200

    bank app

---
+++ bankapp.js
@@ -0,0 +1,14 @@
+const login = () => {
+let newCustomer ={
+    aNumber : accountnumber.value,
+    pword : password.value
+}
+
+    if ((aNumber = accountnumber.value) && (pword = password.value))
+
+    aNumber.value = ""
+    password.value = ""
+
+
+console.log(newCustomer)
+}
